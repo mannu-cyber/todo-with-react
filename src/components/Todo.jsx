90750631204d716f18d9925ccf6c4492337b64b2
Abhimanyu sahani <sahanimannu844@gmail.com>
2024-02-16 08:37:29 +0530

    animataion

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,26 +1,57 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect, Children } from "react";
+import gsap from "gsap";
 
 const Todo = () => {
   const [task, setTask] = useState([]);
-  const [newTask, setNewTask] = useState(" ");
+  const [newTask, setNewTask] = useState("");
+  const [error, setError] = useState("");
+
+  const elementRef = useRef(null);
+
+  const pop = useRef(null)
+useEffect(()=>{
+  gsap.from(pop.current,{
+    scale:0,
+    duration:.5,
+  })
+})
+
+
+  useEffect(() => {
+    if (elementRef.current) {
+      gsap.from(elementRef.current, {
+        y: -60,
+        opacity:0,
+        duration: 1,
+        ease: "power3.out",
+      });
+    }
+  }, [task]);
 
   const addTask = () => {
-    if (newTask === " ") {
-      alert("enter a task");
+    if (newTask.trim() === "") {
+      setError("Please enter a task");
       return;
+    } else if (newTask.length < 5) {
+      setError("Please add more than 4 characters");
+      return;
+    } else {
+      setError("");
     }
+
     setTask([...task, newTask]);
+    setNewTask("");
   };
 
   return (
     <>
-      <div className="main  bg-gradient-to-r mx-auto py-[10vw] from-gray-700 via-gray-900 to-black w-screen h-screen text-white">
-        <div className="todo	overflow-hidden w-[30vw] h-[60vh]  mx-auto rounded-[20px] p-[10px] border">
+      <div className="main bg-gradient-to-r mx-auto flex justify-center items-center from-gray-700 via-gray-900 to-black w-screen h-screen text-white">
+        <div className="todo w-[30vw] h-[70vh] mx-auto rounded-[20px] p-[10px] border">
           <h1 className="text-[3vw] font-bold ms-[7vw] bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 inline-block text-transparent bg-clip-text ">
             ToDo App
           </h1>
 
-          <div className="inputContainer  justify-center flex w-full h-[3vw] items-center gap-[1vw] mt-9">
+          <div className="inputContainer justify-center flex w-full h-[3vw] items-center gap-[1vw] mt-9">
             <input
               value={newTask}
               onChange={(e) => {
@@ -32,24 +63,28 @@ const Todo = () => {
             />
             <button
               onClick={addTask}
-              className="py-[.7vw] px-[2vw] bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 inline-block      rounded-[60px]"
+              className="py-[.7vw] px-[2vw] bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 inline-block rounded-[60px]"
             >
               Add Task
             </button>
           </div>
 
-          <div className="task-container my-[1vw] px-[2vw] w-full flex flex-col gap-2  overflow-y-scroll h-[300px] ">
+          <div ref={elementRef} className="task-container my-[1vw] pt-6 px-[2vw] w-full flex flex-col gap-2 overflow-y-scroll h-[350px] ">
             {task.map((task, i) => {
               return (
-                <h1
+                <h1 
                   key={i}
-                  className="text-[1.5vw] border rounded-[20px] py-1 px-6"
+                  className="hover:scale-[1.06] cursor-pointer duration-[.5s] ease-linear  text-[1.5vw] border rounded-[20px] py-1 px-6"
                 >
                   {task}
                 </h1>
               );
             })}
+           
           </div>
+          <p ref={pop} className="text-center fixed top-[4vw] text-red-600 mt-3 text-3xl">
+              {error}
+            </p>
         </div>
       </div>
     </>
